5e2334ba5cfada125035343caef09d19ba62e3b5
lynnpark77 <raphaie@gmail.com>
2025-09-09 22:28:52 +0900

    current year

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,5 +1,4 @@
-const year = document.querySelector("#currentyear");
+const currentyear = document.querySelector("#currentyear");
 const today = new Date();
-currentyear.innerHTML = `<span class="highlight">${today.getFullYear()}</span>`;
 
-alert(document.lastModified);
+currentyear.innerHTML = ` <span class="highlight">${today.getFullYear()}</span>`;
