8212ed19ee65145e3a082c7cb4c5e5dd0374037d
lynnpark77 <raphaie@gmail.com>
2025-09-12 23:09:28 +0900

    js file complete

--- week02/scripts/scripture-app.js
+++ week02/scripts/scripture-app.js
@@ -1,11 +1,26 @@
 const input = document.querySelector('#favchap');
 const button = document.querySelector('button');
-const list = document.querySelector('');
-const li = document.createElement('li');
-const deleteButton = document.createElement('button');
+const list = document.querySelector('#list');
 
-li.textContent = input.value;
-deleteButton.textContent = '❌'
 
-li.append(deleteButton);
-list.append(li);
+
+
+function addScripture() {
+    const li = document.createElement('li');
+    const deleteButton = document.createElement('button');
+    li.textContent = input.value;
+    deleteButton.textContent = '❌'
+
+    li.append(deleteButton);
+    list.append(li);
+
+    if (input.value.trim() !== "") {
+        return;
+    }
+}
+
+    deleteButton.addEventListener('click',() => {
+    list.removeChild(li);
+    input.focus();
+    
+    });
