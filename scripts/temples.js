b36a57f324a960004bd1be2132ecbdd41f6acc14
lynnpark77 <raphaie@gmail.com>
2025-09-14 06:37:21 +0900

    W2 assignment completed

---
+++ scripts/temples.js
@@ -0,0 +1,26 @@
+const currentyear = document.querySelector("#currentyear");
+const today = new Date();
+
+currentyear.innerHTML = ` <span class="highlight">${today.getFullYear()}</span>`;
+
+let lastModified = new Date(document.lastModified);
+
+let formatted = lastModified.toLocaleString("en-GB", {
+    year: "numeric",
+    month: "2-digit",
+    day: "2-digit",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+    hour12: false
+});
+
+document.getElementById("lastModified").textContent = formatted;
+
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+    navigation.classList.toggle('open');
+    hamButton.classList.toggle('open');
+});
