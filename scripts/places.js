90134667639dfa31036f305f17dfc31396e1ceef
lynnpark77 <raphaie@gmail.com>
2025-09-23 23:11:38 +0900

    wind chill

--- scripts/places.js
+++ scripts/places.js
@@ -17,5 +17,20 @@ let formatted = lastModified.toLocaleString("en-GB", {
 
 document.getElementById("lastModified").textContent = formatted;
 
-const calculateWindChill = document.querySelector("#wind-chill");
+let temperature = 10;
+let windSpeed = 5;
+
+function calculateWindChill(temp, speed) {
+    return (
+        13.12 + 0.6215 * temp - 11.37 * Math.pow(speed,0.16) + 0.3965 * temp * Math.pow(speed,0.16)
+    ).toFixed(1);
+}
+
+let windChill;
+
+if (temperature <= 10 && windSpeed > 4.8) {
+    windChill = calculateWindChill(temperature, windSpeed) + "℃";
+} else { windChill = 'N/A'; }
+
+document.getElementById("windChill").textContent = windChill;
 
