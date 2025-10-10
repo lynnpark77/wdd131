e08c154042d3061d2781822c0a30c7aa53f10993
lynnpark77 <raphaie@gmail.com>
2025-10-10 14:59:49 +0900

    select option completed

---
+++ form/scripts/form.js
@@ -0,0 +1,59 @@
+const currentyear = document.querySelector("#currentyear");
+const today = new Date();
+
+currentyear.innerHTML = ` <span class="highlight">${today.getFullYear()}</span>`;
+
+let lastModified = new Date(document.lastModified);
+
+let formatted = lastModified.toLocaleString("en-GB", {
+    year: "numeric",
+    month: "2-digit",
+    day: "2-digit",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+    hour12: false
+});
+
+document.getElementById("lastModified").textContent = formatted;
+
+const products = [
+    {
+        id: "fc-1888",
+        name: "flux capacitor",
+        averagerating: 4.5
+    },
+    {
+        id: "fc-2050",
+        name: "power laces",
+        averagerating: 4.7
+    },
+    {
+        id: "fs-1987",
+        name: "time circuits",
+        averagerating: 3.5
+    },
+    {
+        id: "ac-2000",
+        name: "low voltage reactor",
+        averagerating: 3.9
+    },
+    {
+        id: "jj-1969",
+        name: "warp equalizer",
+        averagerating: 5.0
+    }
+];
+
+document.addEventListener("DOMContentLoaded", () => {
+    const productSelect = document.getElementById("product");
+
+    products.forEach(product => {
+        const option = document.createElement("option");
+
+        option.value = product.name;
+        option.textContent = product.name;
+
+        productSelect.appendChild(option);
+    })
+})
