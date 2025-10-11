46389274f47d085b3bcfcd6f96f448fe77b05eb0
lynnpark77 <raphaie@gmail.com>
2025-10-11 22:29:46 +0900

    landing site completed

---
+++ project/scripts/project.js
@@ -0,0 +1,27 @@
+const currentyear = document.querySelector("#currentyear");
+const today = new Date();
+
+currentyear.innerHTML = ` <span class="highlight">${today.getFullYear()}</span>`;
+
+let lastModified = new Date(document.lastModified);
+
+let formatted = lastModified.toLocaleString("en-GB", {
+    year: "numeric",
+    month: "2-digit",
+    day: "2-digit",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+    hour12: false
+});
+
+document.getElementById("lastModified").textContent = formatted;
+
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+const header = document.querySelector('header')
+
+hamButton.addEventListener('click', () => {
+    navigation.classList.toggle('open');
+    hamButton.classList.toggle('open');
+});
