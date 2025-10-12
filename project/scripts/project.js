69e25798cd537bd78353e28756be8f2eaae1e745
lynnpark77 <raphaie@gmail.com>
2025-10-12 21:15:56 +0900

    courses intro

--- project/scripts/project.js
+++ project/scripts/project.js
@@ -25,3 +25,17 @@ hamButton.addEventListener('click', () => {
     navigation.classList.toggle('open');
     hamButton.classList.toggle('open');
 });
+
+const visitsDisplay = document.querySelector(".visits");
+
+let numVisits = Number(window.localStorage.getItem("numVisits-ls")) || 0;
+
+if (numVisits !== 0) {
+    visitsDisplay.textContent = numVisits;
+} else {
+    visitsDisplay.textContent = `This is your first visit. 🥳 Welcome!`;
+}
+
+numVisits++;
+
+localStorage.setItem("numVisits-ls", numVisits);
