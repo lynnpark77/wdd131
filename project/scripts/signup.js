f304a2f39f2d8b587e4ca36db7662ef04f07b674
lynnpark77 <raphaie@gmail.com>
2025-10-14 17:06:29 +0900

    sign up page

---
+++ project/scripts/signup.js
@@ -0,0 +1,43 @@
+const currentyear = document.querySelector("#currentyear");
+const today = new Date();
+
+currentyear.innerHTML = ` <span class="highlight">${today.getFullYear()}</span>`;
+
+let lastModified = new Date(document.lastModified);
+
+let formatted = lastModified.toLocaleString("en-GB", {
+    year: "numeric",
+    month: "2-digit",
+    day: "2-digit",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+    hour12: false
+});
+
+document.getElementById("lastModified").textContent = formatted;
+
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+const header = document.querySelector('header')
+
+hamButton.addEventListener('click', () => {
+    navigation.classList.toggle('open');
+    hamButton.classList.toggle('open');
+});
+
+document.querySelector("main").style.display = "block";
+
+const visitsDisplay = document.querySelector(".visits");
+
+let numVisits = Number(window.localStorage.getItem("numVisits-ls")) || 0;
+
+if (numVisits !== 0) {
+    visitsDisplay.textContent = numVisits;
+} else {
+    visitsDisplay.textContent = `This is your first visit. 🥳 Welcome!`;
+}
+
+numVisits++;
+
+localStorage.setItem("numVisits-ls", numVisits);
